130bbe5cda4ae7556b51416acfa87598146c40ce
Istvan Matejcsok <119620946+matejcsok-ee@users.noreply.github.com>
2025-04-09 11:34:17 +0200

    fix: 🐛 inserPreview pos

--- src/insertPreview.ts
+++ src/insertPreview.ts
@@ -1,10 +1,11 @@
 import { EditorView } from "prosemirror-view";
 import { previewPluginKey } from "./types";
+import { EditorState } from "prosemirror-state";
 
 export const insertPreview = (
   view: EditorView,
   url: string,
-  pos: number,
+  findPlaceholder: (state: EditorState, id: object) => number | null,
   id: object,
   callback: (url: string) => Promise<any>
 ) => {
@@ -12,35 +13,42 @@ export const insertPreview = (
     return;
   }
 
-  callback(url).then((data) => {
-    const { title, description, images } = data;
-    if (!images?.[0]) {
-      const textNode = view.state.schema.text(url);
-      const nodeWithMark = textNode.mark([
-        view.state.schema.marks.link.create({ href: url }),
-      ]);
-      view.dispatch(view.state.tr.replaceSelectionWith(nodeWithMark));
+  callback(url)
+    .then((data) => {
+      const { title, description, images } = data;
+      if (!images?.[0]) {
+        const textNode = view.state.schema.text(url);
+        const nodeWithMark = textNode.mark([
+          view.state.schema.marks.link.create({ href: url }),
+        ]);
+        view.dispatch(view.state.tr.replaceSelectionWith(nodeWithMark));
+        view.dispatch(
+          view.state.tr.setMeta(previewPluginKey, { type: "remove", id })
+        );
+        return;
+      }
+      const attrs = {
+        title,
+        description,
+        src: images[0],
+        alt: title,
+        url,
+      };
+      const previewNode = view.state.schema.nodes.preview.create(attrs);
+
+      const pos = findPlaceholder(view.state, id);
+      if (!pos) {
+        return;
+      }
+      view.dispatch(
+        view.state.tr
+          .replaceWith(pos, pos + url.length, previewNode)
+          .setMeta(previewPluginKey, { type: "remove", id })
+      );
+    })
+    .catch(() => {
       view.dispatch(
         view.state.tr.setMeta(previewPluginKey, { type: "remove", id })
       );
-      return;
-    }
-    const attrs = {
-      title,
-      description,
-      src: images[0],
-      alt: title,
-      url,
-    };
-    const previewNode = view.state.schema.nodes.preview.create(attrs);
-
-    if (!pos) {
-      return;
-    }
-    view.dispatch(
-      view.state.tr
-        .replaceWith(pos, pos + url.length, previewNode)
-        .setMeta(previewPluginKey, { type: "remove", id })
-    );
-  });
+    });
 };
